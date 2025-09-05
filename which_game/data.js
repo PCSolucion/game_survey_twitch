d28e49fbd38b2af79896fd9edd3460ac6c5bbde8
Liukin <contacto@pc-solucion.es>
2025-09-05 03:22:45 +0200

    New games list updated

--- which_game/data.js
+++ which_game/data.js
@@ -2,8 +2,8 @@
 // Puedes usar URLs absolutas (http/https) o rutas locales relativas al archivo `index.html`
 // Deben ser exactamente 4 elementos
 window.OPTIONS_DATA = [
-  { title: "Mafia Remake", image: "https://img.redbull.com/images/c_crop,x_1424,y_0,h_2160,w_1620/c_fill,w_450,h_600/q_auto,f_auto/redbullcom/2020/9/23/eyozwwlgiazzxprwbukt/mafia-definitive-edition" },
-  { title: "A plague Tale: Innocence", image: "https://www.superjumpmagazine.com/content/images/2022/10/A-Plague-Tale-Innocence@2000x1270.jpg" },
-  { title: "Tomb Raider 2013", image: "https://ams3.digitaloceanspaces.com/web01.ho-sting/videogamesartwork_com/public/concept-art/1590659716/tombraider2013_definitive_edition_02_by_brenoch_adams_additions_01.jpg" },
-  { title: "Hellblade: Senua’s Sacrifice", image: "https://i0.wp.com/nerfeados.com/wp-content/uploads/2017/10/Hellblade.jpg?fit=1920%2C1080&ssl=1" },
+  { title: "RYSE: SON OF ROME", image: "https://e.snmc.io/lk/f/x/08fd0d38f3c3c010942d61a29696bff6/10575234" },
+  { title: "FINAL FANTASY X", image: "https://i.etsystatic.com/13594991/r/il/bc77fc/2465009882/il_1588xN.2465009882_32pb.jpg" },
+  { title: "DEAD SPACE (2008)", image: "https://cdn.mos.cms.futurecdn.net/KdwGceaYPmKiSbLTp738rS.jpg" },
+  { title: "BIOSHOCK INFINITE", image: "https://erdorin.org/wp-content/uploads/2013/09/Bioshock-Infinite.jpg" },
 ];
