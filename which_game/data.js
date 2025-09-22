0b2b8e8f7e2df6dd34e1348aba648a9fce6151d6
Liukin <contacto@pc-solucion.es>
2025-09-22 06:06:09 +0200

    Update games 22092025

--- which_game/data.js
+++ which_game/data.js
@@ -2,8 +2,8 @@
 // Puedes usar URLs absolutas (http/https) o rutas locales relativas al archivo `index.html`
 // Deben ser exactamente 4 elementos
 window.OPTIONS_DATA = [
-  { title: "DEAD SPACE 2", image: "https://cdn1.epicgames.com/spt-assets/3d281cbdd65142acbf834aeb20f7bc0c/dead-space-2-19wrn.jpg" },
-  { title: "WOLFENSTEIN: THE NEW ORDER", image: "https://image.api.playstation.com/vulcan/ap/rnd/202009/3004/V6rH21cN1JtpOt6Hk9wFBrJS.png" },
-  { title: "METRO 2033", image: "https://m.media-amazon.com/images/M/MV5BMjhiOTVjZWYtODE2MS00NjYxLTk5NTItMDYyYzNlMzE0MjQwXkEyXkFqcGc@._V1_.jpg" },
-  { title: "ASSASINS CREED ORIGINS", image: "https://i.pinimg.com/736x/e3/b9/9c/e3b99cac1ee94a9407fe41866a16e397.jpg" },
+  { title: "DYING LIGHT THE BEAST", image: "https://sm.ign.com/t/ign_latam/screenshot/default/dlbeast-1_vwv4.1280.jpg" },
+  { title: "DEAD SPACE 3", image: "https://i.blogs.es/25ee3a/dead-space-3-1-1-/1366_2000.jpeg" },
+  { title: "FINAL FANTASY XV", image: "https://m.media-amazon.com/images/I/715FBWApFhL._UF1000,1000_QL80_.jpg" },
+  { title: "GHOST RECON WILDLANDS", image: "https://ir.ozone.ru/s3/multimedia-1-q/c1000/7378229834.jpg" },
 ];
