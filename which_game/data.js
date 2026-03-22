96a2ef4184144d55427dddb0783b49d93d6f9b0e
Liukin <contacto@pc-solucion.es>
2026-03-22 17:51:07 +0100

    Change God of War Ragnarok image to a custom one

--- which_game/data.js
+++ which_game/data.js
@@ -2,7 +2,7 @@
 // Puedes usar URLs absolutas (http/https) o rutas locales relativas al archivo `index.html`
 // Deben ser exactamente 3 elementos
 window.OPTIONS_DATA = [
-  { title: "GOD OF WAR RAGNAROK", image: "auto" },
+  { title: "GOD OF WAR RAGNAROK", image: "https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/4xJ8XB3bi888QTLZYdl7Oi0s.png" },
   { title: "METRO 2033 REDUX", image: "auto" },
   { title: "WOLFENSTEIN: THE NEW ORDER", image: "auto" },
 ];
